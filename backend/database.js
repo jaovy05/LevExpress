ade41cefd06fe5d985b1e3158c0b7da921851d36
Gomes <joaovitorg.silva050@gmail.com>
2025-05-06 21:43:12 -0300

    Feat: rotas do entregador. card: #4

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const pgp = require("pg-promise")({});
+// Pegando variáveis do ambiente
+const { USUARIO, HOST, DATABASE, PASSWORD, PORT } = process.env;
 
-const { USER, HOST, DATABASE, PASSWORD, PORT } = process.env;
+// Criando a instância do Sequelize
+const sequelize = new Sequelize(DATABASE, USUARIO, PASSWORD, {
+    host: HOST,
+    dialect: 'postgres',
+    port: PORT,
+    logging: console.log, // Opcional: mostra queries no console
+});
 
-const db = pgp(`postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`);
+// Testando a conexão
+sequelize.authenticate()
+    .then(() => console.log('Conexão com o banco de dados bem-sucedida!'))
+    .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
 
-module.exports = db;
+module.exports = sequelize;
