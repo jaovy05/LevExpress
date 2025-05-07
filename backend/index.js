f0e1cacee53b7d0517f0e4ed6ed6866f556bb140
Bruno de Macedo Sanagiotto <brunomacedosana@gmail.com>
2025-05-07 15:40:11 -0300

    ajuste no sequelize que estava dando erro e rotas no index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,24 @@ const port = 5000;
 const passport = require('passport');
 const cors = require('cors');
 const db = require('./database.js');
-const login = require('./controller/Login.js');
 
 app.use(express.json());
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
   });
-  
+
+/* Entregador */
+const entregador = require('./controller/entregador.js');
+app.post('/entregador', entregador.create);
+app.get('/entregador', entregador.findAll);
+app.get('/entregador/:id', entregador.findOne);
+app.put('/entregador/:id', entregador.update);
+app.delete('/entregador/:id', entregador.delete);
+
+/* Login */
+const login = require('./controller/Login.js');
+
 app.post('/login', login.create);
 app.get('/login', login.login);
 app.put('/login/:id', login.update);
